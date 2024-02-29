33fb03b68f52ed6557653dd78e3c74c3900aeef9
ISAAC SARANGO <isaac.sago99@gmail.com>
2024-02-28 21:20:11 -0500

    Mi primer archivo .py

--- hello_World.py
+++ hello_World.py
@@ -2,5 +2,5 @@ def welcome_message():
     name = input("Escribe tu nombre: ")
     print("Hola ",name," Bienvenido al react!")
     
-    if __name__ == "__main__":
-        welcome_message()
+if __name__ == "__main__":
+    welcome_message()
