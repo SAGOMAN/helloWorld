ee2695414b23031fdb5b3305ee67e3632159c57e
ISAAC SARANGO <isaac.sago99@gmail.com>
2024-03-08 21:49:10 -0500

    Agregar funcionalidad de conocer la ciudad

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,8 @@
 def welcome_message():
-    name = input("Ingrese su nombre: ")
-    print("Bienvenio,", name, "al curso de Django y React!")
+    name = input("Por favor ingrese su nombre: ")
+    ciudad = input("¿De dónde eres? ")
+
+    print("Bienvenido,", name, "al curso de Django y React!")
 
 if __name__ == "__main__":
     welcome_message()
