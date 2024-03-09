727d97985cd9e3c42c4c1134df45a1131309f34e
ISAAC SARANGO <isaac.sago99@gmail.com>
2024-03-08 21:49:47 -0500

    Agregar funcionalidad de registrar lo que más llamó la atención

--- hello_world.py
+++ hello_world.py
@@ -2,5 +2,9 @@ def welcome_message():
     name = input("Ingrese su nombre: ")
     print("Bienvenio,", name, "al curso de Django y React!")
 
+
+
+    atencion = input("¿Qué fue lo que más te llamó la atención del curso? ")
+
 if __name__ == "__main__":
     welcome_message()
